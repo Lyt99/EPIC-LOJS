1932076c0764bf5f2d10b5e7e421a25560179405
Junyi <hji2009@126.com>
2015-11-05 19:20:58 +0800

    重新push

---
+++ EPIC-Core/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EPIC_Core
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Debugger.Init();
+            string path = textBox.Text;
+            if (Debugger.Start(path) == 0)
+                Notifier.CreateToastNotification("成功运行！");
+            else
+                Notifier.CreateToastNotification("成功地失败了");
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
